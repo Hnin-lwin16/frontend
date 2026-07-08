054715d0b2455949f69b49f6024391aec9fbfeb1
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-08 23:18:32 +0630

    home_shop_ui

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,23 @@
 import { createBrowserRouter } from "react-router-dom"
-import Home from "./components/common/Home"
-import Shop from "./components/common/Shop"
+import Home from "./components/Home"
+import Shop from "./components/Shop"
+import Layout from "./components/common/Layout"
 
 const router = createBrowserRouter([
-  { 
-    path: "/",
+ {
+  path: "/",
+  element:<Layout/>,
+  children:[
+     { 
+   index: true,
     element: <Home />,
   },
   {
     path: "/shop",
     element: <Shop />,
   }
+  ]
+ }
 ])
 
 export default router
