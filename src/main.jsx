0ee3af59b6805eee23ae50f6e57603387c9b087c
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-10 15:31:33 +0630

    admin_auth

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,14 @@ import { Route, RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/css/style.scss";
+  import { ToastContainer, toast } from 'react-toastify';
+import { AdminAuthProvider } from './components/context/AdminAuth.jsx'
+
 createRoot(document.getElementById('root')).render(
 <>
-  <RouterProvider router={router} />
-
+  <AdminAuthProvider>
+    <RouterProvider router={router} />
+  </AdminAuthProvider>
+<ToastContainer/>
 </>
 )
