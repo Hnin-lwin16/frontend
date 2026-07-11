e01a453a12a9492ed813e4a661491e48875495d0
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-11 14:59:18 +0630

    brand_crud

---
+++ src/components/admin/brand/Create.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import { token } from '../../common/http';
+import Sidebar from '../../common/Sidebar';
+import { toast } from 'react-toastify';
+
+const Create = () => {
+  const [disable,setDisable] = useState(false);
+  const navigate = useNavigate();
+  // console.log(token());
+   const {
+          register,
+          handleSubmit,
+          watch,
+          formState: {errors},
+      } = useForm();
+      const saveBrand = async (data) => {
+        setDisable(true)
+        console.log(data);
+        const res = fetch(`${import.meta.env.VITE_API_URL}/brands`,{
+                    method: 'POST',
+                    headers:{
+                        'Content-type' : 'application/json',
+                        'Accept' : 'application/json',
+                        'Authorization': `Bearer ${token()}`
+                    },
+                    body: JSON.stringify(data)
+                }).then(res => res.json())
+                .then(result => {
+                  setDisable(false)
+                    
+                    if(result.status == 200){
+                        toast.success(result.message);
+                        navigate("/admin/brands");
+
+                    }else{
+                        console.log("something wrong");
+                    }
+                    // console.log(result);
+                })
+      }
+  return (
+     <div className='container'>
+  <div className=' row'>
+    <div className=' d-flex justify-content-between mt-5 pb-3'>
+      <h4 className="h4 pb-0 mb-0">
+        Brands / Create
+      </h4>
+      <Link to="/admin/brands" className='btn btn-primary'>Back</Link>
+    </div>
+    <div className='col-md-3'>
+      <Sidebar/>
+    </div>
+    <div className='col-md-9'>
+      <form  onSubmit={handleSubmit(saveBrand)}>
+         <div className='card shadow '>
+        <div className="card-body p-4">
+        <div className="mb-3">
+          <label htmlFor="" className=' form-label'>
+          Name
+        </label>
+        <input 
+        {
+          ...register('name',{
+            required: "The name field is required"
+          })
+        }
+        type="text"  className={`form-control ${errors.name && 'is-invalid'}`} placeholder='Name' />
+        {
+          errors.name && <p className='invalid-feedback'>{errors.name?.message}</p>
+        }
+        </div>
+        <div className="mb-3">
+          <label htmlFor="" className=' form-label'>
+          Status
+        </label>
+       <select
+        {
+          ...register('status',{
+            required: "Please select a status"
+          })
+        }
+        className={`form-control ${errors.status && 'is-invalid'}`} >
+        <option value="">Select a Status</option>
+        <option value="1">Active</option>
+        <option value="0">Block</option>
+       </select>
+        {
+          errors.status && <p className='invalid-feedback'>{errors.status?.message}</p>
+        }
+        </div>
+
+        </div>
+     </div>
+     <button type='submit' className='btn btn-primary mt-3'>Create</button>
+      </form>
+    
+      
+    </div>
+  </div>
+ </div>
+  )
+}
+
+export default Create
