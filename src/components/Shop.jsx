134cdc04a563f56f2cf1c34277172705091494b1
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-23 16:18:39 +0630

    category_brand_filter

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,9 +1,121 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductOneImg from "../assets/images/eight.jpg"
-import { Link } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import BreadCrumb from './common/BreadCrumb'
 
 const Shop = () => {
+  const [categories,setCategories] = useState([]);
+  const [brands,setBrands] = useState([]);
+  const[products,setProducts] = useState([]);
+  const [searchParams,setSearchParams] = useSearchParams();
+  const [catChecked,setCatChecked] = useState(()=>{
+    const category = searchParams.get('category');
+    return category ? category.split(',') : [];
+  });
+  const [brandChecked,setBrandChecked] = useState(()=>{
+    const brand = searchParams.get('brand');
+    return brand ? brand.split(',') : [];
+  });
+  
+ const fetchProducts = async () => {
+  let search = []
+  let params = '';
+  if(catChecked.length > 0){
+    search.push(['category',catChecked])
+  }
+  if(search.length > 0){
+     params = new URLSearchParams(search)
+  }
+   if(brandChecked.length > 0){
+    search.push(['brand',brandChecked])
+  }
+  if(search.length > 0){
+     params = new URLSearchParams(search)
+     setSearchParams(params)
+  }else{
+    setSearchParams([])
+  }
+  
+     await fetch(`${import.meta.env.VITE_API_URL}/get-products?${params}`,{
+          method: 'GET',
+          headers:{
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+           
+          }
+        })
+        .then(res => res.json())
+        .then(result => {
+          if(result.status == 200){
+            setProducts(result.data)
+          }else{
+            console.log("Something went wrong");
+          }
+         
+        })
+  }
+  const fetchCategories = async () => {
+     await fetch(`${import.meta.env.VITE_API_URL}/get-categories`,{
+          method: 'GET',
+          headers:{
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+           
+          }
+        })
+        .then(res => res.json())
+        .then(result => {
+          if(result.status == 200){
+            setCategories(result.data)
+          }else{
+            console.log("Something went wrong");
+          }
+         
+        })
+  }
+   const fetchBrands = async () => {
+     await fetch(`${import.meta.env.VITE_API_URL}/get-brands`,{
+          method: 'GET',
+          headers:{
+            'Content-type': 'application/json',
+            'Accept': 'application/json',
+           
+          }
+        })
+        .then(res => res.json())
+        .then(result => {
+          if(result.status == 200){
+            setBrands(result.data)
+          }else{
+            console.log("Something went wrong");
+          }
+         
+        })
+  }
+  const handleCategory = (e) => {
+    const {checked,value} = e.target;
+    if(checked){
+      setCatChecked(pre => [...pre,value])
+    }else{
+      setCatChecked(catChecked.filter(id => id != value))
+    }
+    
+  }
+  const handleBrand = (e) => {
+    const {checked,value} = e.target;
+    if(checked){
+      setBrandChecked(pre => [...pre,value])
+    }else{
+      setBrandChecked(setBrandChecked.filter(id => id != value))
+    }
+    
+  }
+  useEffect(() => {
+    fetchCategories();
+    fetchBrands()
+    fetchProducts()
+
+  },[catChecked,brandChecked])
   return (
    <div className='container'>
     <BreadCrumb currentPageTitle="Shop"/>
@@ -13,18 +125,19 @@ const Shop = () => {
       <div className='card-body p-4'>
       <h3 className="mb-3">Categories</h3>
       <ul>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Kids</label>
+        {
+          categories && categories.map(category =>{
+            return(
+ <li key={`cat-${category.id}`} className='mb-2'>
+          <input defaultChecked={searchParams.get('category')? 
+            searchParams.get('category').includes(category.id):false
+          } type='checkbox' value={category.id} onClick={handleCategory}/>
+          <label className="ps-2">{category.name}</label>
         </li>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Women</label>
-        </li>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Mens</label>
-        </li>
+            )
+          })
+        }
+       
       </ul>
       </div>
     </div>
@@ -32,112 +145,51 @@ const Shop = () => {
       <div className='card-body p-4'>
       <h3 className="mb-3">Brands</h3>
       <ul>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Puma</label>
+        {
+          brands && brands.map(brand =>{
+            return(
+ <li key={`brand-${brand.id}`} className='mb-2'>
+          <input
+          defaultChecked={searchParams.get('brand')? 
+            searchParams.get('brand').includes(brand.id):false
+          }
+          type='checkbox' value={brand.id} onClick={handleBrand}/>
+          <label className="ps-2">{brand.name}</label>
         </li>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Killer</label>
-        </li>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Levis</label>
-        </li>
-        <li className='mb-2'>
-          <input type='checkbox'/>
-          <label className="ps-2">Flying Machine</label>
-        </li>
+            )
+          })
+        }
+       
+        
       </ul>
       </div>
     </div>
   </div>
   <div className="col-md-9">
     <div className='row mt-4'>
-            <div className='col-md-4 col-6'>
+            {
+              products && products.map(product =>{
+                return (
+                   <div className='col-md-4 col-6' key={`product-${product.id}`}>
               <div className='product card border-0'>
                 <div className=' card-img'>
-                  <Link to="/product"><img src={ProductOneImg} className=' w-100'/></Link>
+                  <Link to="/product"><img src={product.image_url} className=' w-100'/></Link>
                 </div>
                 <div className=' card-body pt-3'>
-                  <Link to="/product">Red Check Shirt for Men</Link>
+                  <Link to="/product">{product.title}</Link>
                   <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
+                     ${product.price} 
+                {
+                  product.compare_price &&  <span className=' text-decoration-line-through'> ${product.compare_price}</span>
+                }        
                   </div>
                 </div>
               </div>
              
             </div>
-             <div className='col-md-4 col-6'>
-              <div className='product card border-0'>
-                <div className=' card-img'>
-                  <img src={ProductOneImg} className=' w-100'/>
-                </div>
-                <div className=' card-body pt-3'>
-                  <a href="">Red Check Shirt for Men</a>
-                  <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
-                  </div>
-                </div>
-              </div>
-             
-            </div>
-             <div className='col-md-4 col-6'>
-              <div className='product card border-0'>
-                <div className=' card-img'>
-                  <img src={ProductOneImg} className=' w-100'/>
-                </div>
-                <div className=' card-body pt-3'>
-                  <a href="">Red Check Shirt for Men</a>
-                  <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
-                  </div>
-                </div>
-              </div>
-             
-            </div>
-             <div className='col-md-4 col-6'>
-              <div className='product card border-0'>
-                <div className=' card-img'>
-                  <img src={ProductOneImg} className=' w-100'/>
-                </div>
-                <div className=' card-body pt-3'>
-                  <a href="">Red Check Shirt for Men</a>
-                  <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
-                  </div>
-                </div>
-              </div>
-             
-            </div>
-             <div className='col-md-4 col-6'>
-              <div className='product card border-0'>
-                <div className=' card-img'>
-                  <img src={ProductOneImg} className=' w-100'/>
-                </div>
-                <div className=' card-body pt-3'>
-                  <a href="">Red Check Shirt for Men</a>
-                  <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
-                  </div>
-                </div>
-              </div>
-             
-            </div>
-             <div className='col-md-4 col-6'>
-              <div className='product card border-0'>
-                <div className=' card-img'>
-                  <img src={ProductOneImg} className=' w-100'/>
-                </div>
-                <div className=' card-body pt-3'>
-                  <a href="">Red Check Shirt for Men</a>
-                  <div className=' price'>
-                    $50 <span className=' text-decoration-line-through'>$80</span>
-                  </div>
-                </div>
-              </div>
-             
-            </div>
+                )
+              })
+            }
           </div>
   </div>
 </div>
--- src/components/common/LatestProduct.jsx
+++ src/components/common/LatestProduct.jsx
@@ -36,7 +36,7 @@ const LatestProduct = () => {
         {
           products && products.map(product => {
             return(
-              <div className='col-md-3 col-6' key={product.id}>
+              <div className='col-md-3 col-6' key={`product-${product.id}`}>
           <div className='product card border-0'>
             <div className=' card-img'>
               <img src={product.image_url} className=' w-100'/>
