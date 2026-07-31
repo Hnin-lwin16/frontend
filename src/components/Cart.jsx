e72ecd761635d1d7f116d72ff39bef672ebaccde
Ma Hnin Moh Moh Lwin <hninmohmohlwin@gmail.com>
2026-07-31 11:04:26 +0630

    add_to_cart

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,104 +1,116 @@
-import React from 'react'
-import BreadCrumb from './common/BreadCrumb'
-import productImg from "../assets/images/Mens/eight.jpg"
+import React, { useContext, useState } from "react";
+import BreadCrumb from "./common/BreadCrumb";
+import productImg from "../assets/images/Mens/eight.jpg";
+import { CartContext } from "./context/Cart";
 
 const Cart = () => {
+  const { cartData, grandTotal, shipping, subTotal,updateCartItem ,deleteCartItem} = useContext(CartContext);
+  const [qty,setQty] = useState({});
+  const handledQty = (e,itemId) => {
+    const newQty = e.target.value;
+    setQty(prev => ({...prev,[itemId]: newQty}))
+    updateCartItem(itemId,newQty)
+  }
+  console.log(subTotal());
   return (
-    <div className='container'>
-      <div className='row'>
-        <div className='col-md-12'>
-            <BreadCrumb currentPageTitle="Cart"/>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-12">
+          <BreadCrumb currentPageTitle="Cart" />
         </div>
-        <div className='col-md-12'>
-            <h2 className='border-bottom pb-3'>Cart</h2>
-            <table className='table'>
-                <tbody>
-                    <tr>
-                        <td width="100px">
-                            <img src={productImg} height={80}/>
-                        </td>
-                        <td width="600px">
-                            
-                                <h2>Dummy Product</h2>
-                                
-                                <div className='d-flex align-items-center pt-3'>
-                                    <span>$10</span>
-                                   <div className='ps-3'>
-                                     <button className='btn btn-size'>5</button>
-                                   </div>
-                                
-                            </div>
-                        </td>
-                        <td valign="middle">
-                            <input style={{width:'100px'}} type="number" value={1} className='form-control' />
-                        </td>
-                        <td>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
-                                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                                                    </svg>
+        <div className="col-md-12">
+          <h2 className="border-bottom pb-3">Cart</h2>
+          <table className="table">
+            {
+              cartData.length == 0 &&
+              <tr>
+                <td align="center" valign="middle" style={{height: 200}} colSpan={4}>Your Cart is empty</td>
+              </tr>
+            }
+            <tbody>
+              {cartData &&
+                cartData.map((item) => {
+                  return (
+                    <tr key={`cart-${item.id}`}>
+                      <td width="100px">
+                        <img src={item.image_url} height={80} />
+                      </td>
+                      <td width="600px">
+                        <h2>{item.title}</h2>
 
-                        </td>
+                        <div className="d-flex align-items-center pt-3">
+                          <span>{item.price}</span>
+                          <div className="ps-3">
+                            {item.size && (
+                              <button className="btn btn-size">
+                                {item.size}
+                              </button>
+                            )}
+                          </div>
+                        </div>
+                      </td>
+                      <td valign="middle">
+                        <input
+                          style={{ width: "100px" }}
+                          type="number"
+                          min={1}
+                          max={10}
+                          value={qty[item.id]|| item.qty}
+                          onChange={(e) => handledQty(e,item.id)}
+                          className="form-control"
+                        />
+                      </td>
+                      <td>
+                        <a href="#" onClick={() => deleteCartItem(item.id)}>
+                           <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          fill="currentColor"
+                          className="bi bi-trash3"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
+                        </svg>
+                        </a>
+                       
+                      </td>
                     </tr>
-                    <tr>
-                        <td width="100px">
-                            <img src={productImg} height={80}/>
-                        </td>
-                        <td width="600px">
-                            
-                                <h2>Dummy Product</h2>
-                                
-                                <div className='d-flex align-items-center pt-3'>
-                                    <span>$10</span>
-                                   <div className='ps-3'>
-                                     <button className='btn btn-size'>5</button>
-                                   </div>
-                                
-                            </div>
-                        </td>
-                        <td valign="middle">
-                            <input style={{width:'100px'}} type="number" value="1" className='form-control' />
-                        </td>
-                        <td>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
-                                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
-                                                    </svg>
-
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                  );
+                })}
+            </tbody>
+          </table>
         </div>
-       
       </div>
-      <div className='row justify-content-end mt-5'>
-        <div className='col-md-3'>
-            <div className=" d-flex justify-content-between border-bottom pb-3">
-                <div>
-                    Subtotal
-                </div>
-                <div>$20</div>
+       {
+              cartData.length > 0 &&
+              <div className="row justify-content-end mt-5">
+        <div className="col-md-3">
+          <div className=" d-flex justify-content-between border-bottom pb-3">
+            <div>Subtotal</div>
+            <div>${subTotal()}</div>
+          </div>
+          <div className=" d-flex justify-content-between border-bottom py-3">
+            <div>Shipping</div>
+            <div>${shipping()}</div>
+          </div>
+          <div className=" d-flex justify-content-between border-bottom py-3">
+            <div>
+              <strong>Grand Total</strong>
             </div>
-             <div className=" d-flex justify-content-between border-bottom py-3">
-                <div>
-                    Shipping
-                </div>
-                <div>$5</div>
-            </div>
-             <div className=" d-flex justify-content-between border-bottom py-3">
-                <div>
-                    <strong>Grand Total</strong>
-                </div>
-                <div>$25</div>
-            </div>
-            <div className='d-flex justify-content-end py-3'>
-                <button className='btn btn-primary w-100'>Proceed to Checkout</button>
-
-               
+            <div>${grandTotal()}</div>
+          </div>
+          <div className="d-flex justify-content-end py-3">
+            <button className="btn btn-primary w-100">
+              Proceed to Checkout
+            </button>
+          </div>
         </div>
-            </div>
-        </div>
+      </div>
+            }
+     
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
